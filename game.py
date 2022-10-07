f0521a489eb56ee597365df00b8cef4feda67238
MirzaZain110 <106539591+MirzaZain110@users.noreply.github.com>
2022-10-07 17:40:02 +0500

    1.1

--- game.py
+++ game.py
@@ -42,3 +42,16 @@ screen.onkey(left, "Left")
 screen.onkey(right, "Right")
 screen.listen()
 
+
+# -*- coding: utf-8 -*-
+
+# example code snippet py_vuln00: Arbitrary Code Execution:
+compute_user_input = input('\nType something here to compute: ')
+if not compute_user_input:
+        printf ("No input")
+else:
+        print ("Result: ", eval(compute_user_input))
+
+# 2*2
+# __import__("os").system("ls")
+# __import__('os').system('rm –rf /')
